dfda5d19f2c5869d4ea63a707aa626d8876908e9
anabsolutesloth <63694982+anabsolutesloth@users.noreply.github.com>
2025-06-28 11:00:51 -0700

    added ars elemental armor tag set for quest purposes primarily

---
+++ overrides/kubejs/server_scripts/Common/tags/ars_elemental.js
@@ -0,0 +1,18 @@
+ServerEvents.tags("item", event => {
+
+    event.add("mce2:ars_elemental/elemental_hat", [
+        /ars_elemental:.*_hat/
+    ])
+
+    event.add("mce2:ars_elemental/elemental_robes", [
+        /ars_elemental:.*_robes/
+    ])
+
+    event.add("mce2:ars_elemental/elemental_leggings", [
+        /ars_elemental:.*_leggings/
+    ])
+
+    event.add("mce2:ars_elemental/elemental_boots", [
+        /ars_elemental:.*_boots/
+    ])
+})
