1d38db3adc3cb1a6739fc80dc5afc1860d6dd33e
Rocksenburg <rocksenburg@gmail.com>
2025-07-02 18:36:10 -0700

    Update overrides/kubejs/server_scripts/Common/tags/FTB_chunk_tags.js
    Co-authored-by: anabsolutesloth <63694982+anabsolutesloth@users.noreply.github.com>

--- overrides/kubejs/server_scripts/Common/tags/FTB_chunk_tags.js
+++ overrides/kubejs/server_scripts/Common/tags/FTB_chunk_tags.js
@@ -1,5 +1,9 @@
 ServerEvents.tags("entity_type", event => {
-    event.add('ftbchunks:entity_mob_griefing_blacklist', 'specialmobs:spitfireskeleton');
+    event.add('ftbchunks:entity_mob_griefing_blacklist', [
+        'specialmobs:spitfireskeleton',
+        /iceandfire:.*_dragon/,
+        'rats:rat'
+     ]);
     event.add('ftbchunks:entity_mob_griefing_blacklist', 'iceandfire:fire_dragon');
     event.add('ftbchunks:entity_mob_griefing_blacklist', 'iceandfire:ice_dragon');
     event.add('ftbchunks:entity_mob_griefing_blacklist', 'iceandfire:lightning_dragon');
