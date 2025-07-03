ed3f8fb91ae543dd745f448253071a80438ee156
Rocksenburg <rocksenburg@gmail.com>
2025-07-02 18:37:19 -0700

    Update FTB_chunk_tags.js

--- overrides/kubejs/server_scripts/Common/tags/FTB_chunk_tags.js
+++ overrides/kubejs/server_scripts/Common/tags/FTB_chunk_tags.js
@@ -4,8 +4,4 @@ ServerEvents.tags("entity_type", event => {
         /iceandfire:.*_dragon/,
         'rats:rat'
      ]);
-    event.add('ftbchunks:entity_mob_griefing_blacklist', 'iceandfire:fire_dragon');
-    event.add('ftbchunks:entity_mob_griefing_blacklist', 'iceandfire:ice_dragon');
-    event.add('ftbchunks:entity_mob_griefing_blacklist', 'iceandfire:lightning_dragon');
-    event.add('ftbchunks:entity_mob_griefing_blacklist', 'rats:rat');
 })
