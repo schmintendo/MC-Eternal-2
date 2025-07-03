990149ef5d93d60a23625b2d9148533bdacba603
Rocksenburg <rocksenburg@gmail.com>
2025-07-02 17:56:24 -0700

    feat: introduced FTB_chunk_tags.js
    - Can be used to add FTB chunk tags such as mob griefing blacklist
    
    - Dragons now respect claims, but can still break blocks in unclaimed areas
    
    - Rats will respect claims, no more rat tunnels. Take that Barry!
    
    - Spitfire Skeletons won't burn down claimed areas anymore

---
+++ overrides/kubejs/server_scripts/Common/tags/FTB_chunk_tags.js
@@ -0,0 +1,7 @@
+ServerEvents.tags("entity_type", event => {
+    event.add('ftbchunks:entity_mob_griefing_blacklist', 'specialmobs:spitfireskeleton');
+    event.add('ftbchunks:entity_mob_griefing_blacklist', 'iceandfire:fire_dragon');
+    event.add('ftbchunks:entity_mob_griefing_blacklist', 'iceandfire:ice_dragon');
+    event.add('ftbchunks:entity_mob_griefing_blacklist', 'iceandfire:lightning_dragon');
+    event.add('ftbchunks:entity_mob_griefing_blacklist', 'rats:rat');
+})
