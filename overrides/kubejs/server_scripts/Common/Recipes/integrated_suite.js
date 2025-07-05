a61c9a2ee543f3cd4306274bc68d6c66dca7ea66
anabsolutesloth <63694982+anabsolutesloth@users.noreply.github.com>
2025-07-05 13:24:21 -0700

    ID omniconnectors now require End because i'm evil

--- overrides/kubejs/server_scripts/Common/Recipes/integrated_suite.js
+++ overrides/kubejs/server_scripts/Common/Recipes/integrated_suite.js
@@ -11,4 +11,53 @@ ServerEvents.recipes(event => {
         E: "mekanism:elite_control_circuit",
         S: "minecraft:echo_shard"
     }).id("integratedterminals:crafting/terminal_storage_portable")
+
+
+    event.custom({
+        "type": "integrateddynamics:crafting_special_shaped_omni_directional",
+        "group": "integrateddynamics:part_connector_omni_directional",
+        "pattern": [
+            "WLW",
+            "MCM",
+            "WLW"
+        ],
+        "key": {
+            "L": {
+            "item": "integrateddynamics:logic_director"
+            },
+            "M": {
+            "item": "integrateddynamics:part_connector_mono_directional"
+            },
+            "C": {
+            "item": "integrateddynamics:cable"
+            },
+            "W": {
+                "item": "waystones:warp_dust"
+            }
+        }
+    }).id("integrateddynamics:special/part_connector_omni_directional")
+
+    event.custom({
+        "type": "integrateddynamics:crafting_special_shaped_omni_directional_3",
+        "group": "integrateddynamics:part_connector_omni_directional",
+        "pattern": [
+            "WLW",
+            "MOM",
+            "WLW"
+        ],
+        "key": {
+            "L": {
+            "item": "integrateddynamics:logic_director"
+            },
+            "M": {
+            "item": "integrateddynamics:part_connector_mono_directional"
+            },
+            "O": {
+            "item": "integrateddynamics:part_connector_omni_directional"
+            },
+            "W": {
+                "item": "waystones:warp_dust"
+            }
+        }
+    }).id("integrateddynamics:special/part_connector_omni_directional_copy")
 })
