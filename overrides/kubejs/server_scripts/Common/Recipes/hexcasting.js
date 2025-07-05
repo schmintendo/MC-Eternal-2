d59c21f08f456fc891ab8c0bfecdd4f5c8c9599a
anabsolutesloth <63694982+anabsolutesloth@users.noreply.github.com>
2025-07-04 17:09:57 -0700

    made a bunch of Hexcasting decorative blocks craftable they're just, fucking not for some reason?? feels like someone forgot, but oh well easy enough to fix.

--- overrides/kubejs/server_scripts/Common/Recipes/hexcasting.js
+++ overrides/kubejs/server_scripts/Common/Recipes/hexcasting.js
@@ -1,3 +1,32 @@
+
+const hexBlocks = {
+        amethyst: [
+            "minecraft:amethyst_block",
+            "hexcasting:amethyst_bricks",
+            "hexcasting:amethyst_bricks_small",
+            "hexcasting:amethyst_pillar"
+        ],
+        slate: [
+            "hexcasting:slate_block",
+            "hexcasting:slate_tiles",
+            "hexcasting:slate_bricks",
+            "hexcasting:slate_bricks_small",
+            "hexcasting:slate_pillar"
+        ],
+        slate_amethyst: [
+            "hexcasting:slate_amethyst_tiles",
+            "hexcasting:slate_amethyst_bricks",
+            "hexcasting:slate_amethyst_bricks_small",
+            "hexcasting:slate_amethyst_pillar"
+        ],
+        quenched_allay: [
+            "hexcasting:quenched_allay",
+            "hexcasting:quenched_allay_tiles",
+            "hexcasting:quenched_allay_bricks",
+            "hexcasting:quenched_allay_bricks_small"
+        ]
+    }
+
 ServerEvents.recipes(event => {
 
     event.shaped("hexcasting:spellbook", [
@@ -10,4 +39,23 @@ ServerEvents.recipes(event => {
         C: "#forge:ingots/cobalt",
         D: "#forge:ingots/netherite_scrap"
     }).id("hexcasting:spellbook")
+
+    //Deco blocks
+    event.shaped(Item.of("hexcasting:slate_amethyst_tiles", 4), [
+        "SA",
+        "AS"
+    ], {
+        S: "hexcasting:slate_block",
+        A: "#forge:storage_blocks/amethyst"
+    }).id("mce2:hexcasting/slate_amethyst_tiles")
+
+    Object.keys(hexBlocks).forEach(blockSet => {
+        hexBlocks[blockSet].forEach(block => {
+            hexBlocks[blockSet].forEach(inputBlock => {
+                console.log(`${block} -> ${inputBlock}`)
+                if(inputBlock != block)
+                    event.stonecutting(block, inputBlock).id(`mce2:${block.replace(":", "/")}_from_${inputBlock.replace(":", "_")}`)
+            })
+        })
+    })
 })
