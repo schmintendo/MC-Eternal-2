1d6a9f58b7a5a96668ba2ad05c16c19869dccc0d
anabsolutesloth <63694982+anabsolutesloth@users.noreply.github.com>
2025-07-06 18:01:29 -0700

    made AE2WTLib Quantum Bridge Card expensive it needs to be in line with the other crossdim wireless accesses >:V

---
+++ overrides/kubejs/server_scripts/Common/Recipes/ae2.js
@@ -0,0 +1,12 @@
+ServerEvents.recipes(event => {
+
+    event.shaped("ae2wtlib:quantum_bridge_card", [
+        "CAW",
+        " E "
+    ], {
+        A: "ae2:advanced_card",
+        W: "waystones:warp_stone",
+        C: "mekanism:elite_control_circuit",
+        E: "minecraft:echo_shard"
+    }).id("ae2wtlib:quantum_bridge_card")
+})
